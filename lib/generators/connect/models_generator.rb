939bfb5c852484adc1175984a3ec6a92afaa2356
Daniel Heath <daniel.heath@reinteractive.net>
2018-12-21 12:13:21 +1100

    Add email scope to contact which removes whitespace/capitalization
    Fixes #1

--- lib/generators/connect/models_generator.rb
+++ lib/generators/connect/models_generator.rb
@@ -8,51 +8,59 @@ module Connect
 
     def create_account
       create_file "#{directory}/account.rb", <<-FILE
-  module #{modname}
-    class Account < ApplicationRecord
-      require 'connect/record'
-      include Connect::Record
-      self.table_name = "salesforce.account"
+module #{modname}
+  class Account < ApplicationRecord
+    require 'connect/record'
+    include Connect::Record
+    self.table_name = "salesforce.account"
 
-      has_many :salesforce_contacts, class_name: "#{modname}::Contact",
-        foreign_key: :accountid, primary_key: :sfid, dependent: :nullify
+    has_many :salesforce_contacts, class_name: "#{modname}::Contact",
+      foreign_key: :accountid, primary_key: :sfid, dependent: :nullify
 
-      has_one :salesforce_contract, class_name: "#{modname}::Contract",
-        foreign_key: :accountid, primary_key: :sfid, dependent: :nullify
-    end
+    has_one :salesforce_contract, class_name: "#{modname}::Contract",
+      foreign_key: :accountid, primary_key: :sfid, dependent: :nullify
   end
+end
       FILE
     end
 
     def create_contact
       create_file "#{directory}/contact.rb", <<-FILE
-  module #{modname}
-    class Contact < ApplicationRecord
-      require 'connect/record'
-      include Connect::Record
-      self.table_name = "salesforce.contact"
+module #{modname}
+  class Contact < ApplicationRecord
+    require 'connect/record'
+    include Connect::Record
+    self.table_name = "salesforce.contact"
 
-      validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 
-      belongs_to :salesforce_account, class_name: "Salesforce::Account",
-        primary_key: :sfid, foreign_key: :accountid, optional: true
+    belongs_to :salesforce_account, class_name: "Salesforce::Account",
+      primary_key: :sfid, foreign_key: :accountid, optional: true
+
+    scope :by_email, -> (email) {
+      where('LOWER(TRIM(email)) = LOWER(TRIM(?))', email)
+    }
+
+    def email=(value)
+      self[:email] = value.to_s.strip.downcase
     end
   end
+end
       FILE
     end
 
     def create_contract
       create_file "#{directory}/contract.rb", <<-FILE
-  module #{modname}
-    class Contract < ApplicationRecord
-      require 'connect/record'
-      include Connect::Record
-      self.table_name = "salesforce.contract"
+module #{modname}
+  class Contract < ApplicationRecord
+    require 'connect/record'
+    include Connect::Record
+    self.table_name = "salesforce.contract"
 
-      belongs_to :salesforce_account, class_name: "Salesforce::Account",
-        primary_key: :sfid, foreign_key: :accountid
-    end
+    belongs_to :salesforce_account, class_name: "Salesforce::Account",
+      primary_key: :sfid, foreign_key: :accountid
   end
+end
       FILE
     end
 
